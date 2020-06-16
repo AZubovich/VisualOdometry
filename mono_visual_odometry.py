de1dbe5193a9e53a14ab227d63b9f888420cdb9b
AZubovich <zubovich.2000god@gmail.com>
2020-06-16 12:05:05 +0300

    add LKT optical flow matcher

--- mono_visual_odometry.py
+++ mono_visual_odometry.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 
 class MonoVisualOdometry:
 
@@ -8,25 +10,43 @@ class MonoVisualOdometry:
 		self.true_poses = true_poses
 		self.old_image = None
 		self.new_image = None
+		self.key_points = None
+		self.fast = cv2.FastFeatureDetector_create(threshold=20)
+		self.orb = cv2.ORB_create()
+		self.lk_params = dict( winSize  = (15,15), maxLevel = 2, 
+			criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
 	def initial_image(self,img):
 		self.new_image = img
+		self.mask = np.zeros_like(img)
+		self.color = np.random.randint(0,255,(8000,3))
 		self.FAST_detector(img)
 
 	def perform(self, img):
 		self.old_image = self.new_image
 		self.new_image = img
-		self.FAST_detector(img)
+		self.LKT_optical_flow()
 
 	def FAST_detector(self,img):
-		fast = cv2.FastFeatureDetector_create(threshold=20)
-		kp = fast.detect(img,None)
-		img1 = cv2.drawKeypoints(img, kp, None, color=(0,255,0))
-		cv2.imshow('FAST algorithm', img1)
+		kp = self.fast.detect(img,None)
+		self.key_points = np.array([x.pt for x in kp], dtype=np.float32).reshape(-1, 1, 2)
 
 	def ORB_detector(self, img):
-		orb = cv2.ORB_create()
-		kp, des = orb.detectAndCompute(img,None)
-		img1 = cv2.drawKeypoints(img, kp, None, color=(0,255,0))
-		cv2.imshow('ORB algorithm', img1)
+		kp, des = self.orb.detectAndCompute(img,None)
+		self.key_points = np.array([x.pt for x in kp], dtype=np.float32).reshape(-1, 1, 2)
 
+	def LKT_optical_flow(self):
+		p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_image, self.new_image, self.key_points, None, **self.lk_params)
+
+		good_new = p1[st==1]
+		good_old = self.key_points[st==1]
+
+		for i,(new,old) in enumerate(zip(good_new, good_old)):
+			a,b = new.ravel()
+			c,d = old.ravel()
+			self.mask = cv2.line(self.mask, (a,b),(c,d), self.color[i].tolist(), 2)
+			self.new_image = cv2.circle(self.new_image,(a,b),5, self.color[i].tolist(),-1)
+    	
+		img = cv2.add(self.new_image, self.mask)
+		cv2.imshow('LKT optical flow',img)
+		self.key_points = good_new.reshape(-1,1,2)
