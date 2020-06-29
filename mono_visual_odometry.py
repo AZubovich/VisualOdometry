858e5dfc3e411d17b93479171744fe60a36bd394
AZubovich <zubovich.2000god@gmail.com>
2020-06-29 13:48:57 +0300

    add pose estimation

--- mono_visual_odometry.py
+++ mono_visual_odometry.py
@@ -5,26 +5,28 @@ import numpy as np
 class MonoVisualOdometry:
 
 	def __init__(self, calibration_data, true_poses):
-		self.focal_length = calibration_data[1]
-		self.principal_point = (calibration_data[3], calibration_data[7])
+		self.focal_length = float(calibration_data[1])
+		self.principal_point = (float(calibration_data[3]), float(calibration_data[7]))
 		self.true_poses = true_poses
 		self.old_image = None
 		self.new_image = None
 		self.key_points = None
+		self.R = np.zeros(shape=(3, 3))
+		self.t = np.zeros(shape=(3, 3))
 		self.fast = cv2.FastFeatureDetector_create(threshold=20)
 		self.orb = cv2.ORB_create()
 		self.lk_params = dict( winSize  = (15,15), maxLevel = 2, 
 			criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-	def initial_image(self,img):
+	def initial_image(self, img, count):
 		self.new_image = img
-		self.mask = np.zeros_like(img)
-		self.color = np.random.randint(0,255,(8000,3))
+		self.count = count
 		self.FAST_detector(img)
 
-	def perform(self, img):
+	def perform(self, img, count):
 		self.old_image = self.new_image
 		self.new_image = img
+		self.count = count
 		self.LKT_optical_flow()
 
 	def FAST_detector(self,img):
@@ -36,17 +38,52 @@ class MonoVisualOdometry:
 		self.key_points = np.array([x.pt for x in kp], dtype=np.float32).reshape(-1, 1, 2)
 
 	def LKT_optical_flow(self):
-		p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_image, self.new_image, self.key_points, None, **self.lk_params)
 
+		if self.key_points.shape[0] < 1800:
+			self.FAST_detector(self.old_image)
+
+		p1, st, err = cv2.calcOpticalFlowPyrLK(self.old_image, self.new_image, self.key_points, None, **self.lk_params)
 		good_new = p1[st==1]
 		good_old = self.key_points[st==1]
+		self.estimate_pose(good_old, good_new)
 
-		for i,(new,old) in enumerate(zip(good_new, good_old)):
-			a,b = new.ravel()
-			c,d = old.ravel()
-			self.mask = cv2.line(self.mask, (a,b),(c,d), self.color[i].tolist(), 2)
-			self.new_image = cv2.circle(self.new_image,(a,b),5, self.color[i].tolist(),-1)
-    	
-		img = cv2.add(self.new_image, self.mask)
-		cv2.imshow('LKT optical flow',img)
+	def estimate_pose(self, good_old, good_new):
+
+		if self.count < 2:
+			E, mask = cv2.findEssentialMat(good_old, good_new, self.focal_length,
+                         self.principal_point, 
+                         cv2.RANSAC, 
+                         0.999, 
+                         0.1)
+
+			self.R, R2, self.t = cv2.decomposeEssentialMat(E)
+
+		else:
+			E, mask = cv2.findEssentialMat(good_old, good_new, self.focal_length, self.principal_point, cv2.RANSAC, 0.999, 0.1)
+			R1, R2, t = cv2.decomposeEssentialMat(E)
+
+			scale = 1
+			self.R = self.R.dot(R1)
+			self.t = self.t + scale*self.R.dot(t)
+
+
 		self.key_points = good_new.reshape(-1,1,2)
+
+	def estimate_coordinates(self):
+
+		diag = np.array([[-1, 0, 0],
+                        [0, -1, 0],
+                        [0, 0, -1]])
+
+		coord = np.matmul(diag, self.t)
+		return coord.flatten()
+
+	def MSE_error(self, estimated):
+		x = self.true_poses[self.count][3]
+		y = self.true_poses[self.count][7]
+		z = self.true_poses[self.count][11]
+
+		truth = np.array([[x], [y], [z]])
+
+		return np.linalg.norm(truth - estimated)
+
--- visual_odometry.py
+++ visual_odometry.py
@@ -24,6 +24,9 @@ sleep_time = 0
 start_point = (int(round(true_poses[count][3] + 400)), int(round(true_poses[count][11] + 100)))
 end_point = (int(round(true_poses[count][3] + 400)), int(round(true_poses[count][11] + 100)))
 
+est_start_point = start_point
+est_end_point = end_point
+
 
 for imagePath in sorted(paths.list_images(KITTI_path)):
 	img = cv2.imread(imagePath)
@@ -37,25 +40,31 @@ for imagePath in sorted(paths.list_images(KITTI_path)):
 
 	cv2.imshow("KITTI dataset", img)
 
-	cv2.putText(trajectory, "True position" ,(40, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,0,255), 1)
-	cv2.putText(trajectory, "Estimated position", (40, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,255,0), 1)
+	cv2.putText(trajectory, "True position" ,(40, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 0, 255), 1)
+	cv2.putText(trajectory, "Estimated position", (40, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0, 255, 0), 1)
 
 	if count == 0:
-		vo.initial_image(img)
+		vo.initial_image(img, count)
 	else:
-		vo.perform(img)
+		vo.perform(img, count)
+		est_coord = vo.estimate_coordinates()
+		#print(vo.MSE_error(est_coord))
+		est_end_point = (int(round(est_coord[0] + 400)), int(round(est_coord[2] + 100)))
+
 
 	trajectory = cv2.line(trajectory, start_point, end_point, (0, 0, 255), 1)
+	trajectory = cv2.line(trajectory, est_start_point, est_end_point, (0, 255, 0), 1)
 	
 	cv2.imshow("trajectory", trajectory)
 
-	print(f'True x : {true_poses[count][3]}, y : {true_poses[count][7]}, z: {true_poses[count][11]}')
+	#print(f'True x : {true_poses[count][3]}, y : {true_poses[count][7]}, z: {true_poses[count][11]}')
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		print('Its over, buy')
 		break
 
 	count += 1
+	est_start_point = est_end_point
 
 cv2.destroyAllWindows()
 	
